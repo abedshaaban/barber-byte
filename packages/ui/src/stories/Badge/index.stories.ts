09012de5b8b0b26f38b1b78651bb7312927e7a0e
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-04 17:12:26 +0200

    refactor(storybook): created badge

---
+++ packages/ui/src/stories/Badge/index.stories.ts
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import Badge from './component'
+
+const meta: Meta<typeof Badge> = {
+  title: 'components/Badge',
+  component: Badge,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['default', 'secondary', 'destructive', 'outline']
+    }
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof Badge>
+
+export const Default: Story = {
+  args: {
+    text: 'Badge'
+  }
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    text: 'Secondary'
+  }
+}
+
+export const Destructive: Story = {
+  args: { variant: 'destructive', text: 'Destructive' }
+}
+
+export const Outline: Story = {
+  args: { variant: 'outline', text: 'Outline' }
+}
