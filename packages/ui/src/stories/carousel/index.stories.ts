300780468454599ef2ac96075b1804ba9a6e6630
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-04 17:42:49 +0200

    refactor(storybook): created carousel

---
+++ packages/ui/src/stories/carousel/index.stories.ts
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import Carousel from './component'
+
+const meta: Meta<typeof Carousel> = {
+  title: 'components/Carousel',
+  component: Carousel,
+  tags: ['autodocs'],
+  argTypes: {
+    orientation: {
+      control: 'select',
+      options: ['horizontal', 'vertical']
+    }
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof Carousel>
+
+export const Horizontal: Story = {
+  args: {
+    orientation: 'horizontal',
+    loop: false
+  }
+}
+
+export const Vertical: Story = {
+  args: {
+    orientation: 'vertical',
+    loop: false
+  }
+}
+
+export const Loop: Story = {
+  args: { orientation: 'vertical', loop: true }
+}
+
+export const NOLoop: Story = {
+  args: { orientation: 'vertical', loop: false }
+}
