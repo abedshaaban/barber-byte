18e048040ee40699086f945ced86a3c8e0c80782
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-04 16:53:08 +0200

    refactor(storybook): created aspect ration

---
+++ packages/ui/src/stories/AspectRatio/component.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { AspectRatio } from '../../aspect-ratio'
+import Gamer from '../@assets/gamer.png'
+
+type Props = {
+  ratio: number
+}
+
+export default function Index({ ratio }: Props) {
+  return (
+    <div className="w-[450px]">
+      <AspectRatio ratio={ratio}>
+        <img src={Gamer} alt="Image" className="rounded-md object-cover" />
+      </AspectRatio>
+    </div>
+  )
+}
---
+++ packages/ui/src/stories/AspectRatio/index.stories.ts
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import AspectRatio from './component'
+
+const meta: Meta<typeof AspectRatio> = {
+  title: 'components/Aspect Ratio',
+  component: AspectRatio,
+  tags: ['autodocs'],
+  argTypes: {}
+}
+
+export default meta
+
+type Story = StoryObj<typeof AspectRatio>
+
+export const Base: Story = {
+  args: {
+    ratio: 16 / 9
+  }
+}
