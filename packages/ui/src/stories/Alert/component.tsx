e8b160ae9f8275765f069aba09d9eed6067a15c3
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-04 16:39:08 +0200

    refactor(storybook): created alert

---
+++ packages/ui/src/stories/Alert/component.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { RocketIcon } from '@radix-ui/react-icons'
+
+import { Alert, AlertDescription, AlertTitle } from '../../alert'
+
+type Props = {
+  variant: 'default' | 'destructive'
+  title: string
+  description: string
+}
+
+export default function Index({ variant, title, description }: Props) {
+  return (
+    <Alert variant={variant}>
+      <RocketIcon className="h-4 w-4" />
+
+      <AlertTitle>{title}</AlertTitle>
+      <AlertDescription>{description}</AlertDescription>
+    </Alert>
+  )
+}
---
+++ packages/ui/src/stories/Alert/index.stories.ts
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import Alert from './component'
+
+const meta: Meta<typeof Alert> = {
+  title: 'components/Alert',
+  component: Alert,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['default', 'destructive']
+    }
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof Alert>
+
+export const Default: Story = {
+  args: {
+    variant: 'default',
+    title: 'Heads up!',
+    description: 'You can add components and dependencies to your app using the cli.'
+  }
+}
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+    title: 'Error',
+    description: 'Your session has expired. Please log in again.'
+  }
+}
