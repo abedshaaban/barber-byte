06007030182be61659bbe1c31bf8931674fc8e5a
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-04 16:28:44 +0200

    refactor(storybook): created alert dialog

---
+++ packages/ui/src/stories/AlertDialog/index.stories.ts
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import AlertDialog from './component'
+
+const meta: Meta<typeof AlertDialog> = {
+  title: 'components/Alert Dialog',
+  component: AlertDialog,
+  tags: ['autodocs'],
+  argTypes: {}
+}
+
+export default meta
+
+type Story = StoryObj<typeof AlertDialog>
+
+export const Base: Story = {
+  args: {
+    trigger: 'Open',
+    title: 'Are you absolutely sure?',
+    description:
+      'This action cannot be undone. This will permanently delete your account and remove your data from our servers.'
+  }
+}
