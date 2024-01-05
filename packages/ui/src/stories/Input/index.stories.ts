e0d1d5846f565800ba6e91aa9f522838e29244ae
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-06 00:02:11 +0200

    refactor(frontend ui-package): created input storybook

---
+++ packages/ui/src/stories/Input/index.stories.ts
@@ -0,0 +1,49 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import Input from './component'
+
+const meta: Meta<typeof Input> = {
+  title: 'components/Input',
+  component: Input,
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      control: 'select',
+      options: [
+        'button',
+        'checkbox',
+        'color',
+        'date',
+        'datetime-local',
+        'email',
+        'file',
+        'hidden',
+        'image',
+        'month',
+        'number',
+        'password',
+        'radio',
+        'range',
+        'reset',
+        'search',
+        'submit',
+        'tel',
+        'text',
+        'time',
+        'url',
+        'week'
+      ]
+    }
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof Input>
+
+export const Base: Story = {
+  args: {
+    type: 'email',
+    placeholder: 'Email'
+  }
+}
