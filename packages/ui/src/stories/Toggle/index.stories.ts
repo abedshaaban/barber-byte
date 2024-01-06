33c4b4cd20ffa83ee69151099d6d5cec05ef45ea
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-07 00:26:08 +0200

    refactor(frontend ui-package): created toggle storybook

---
+++ packages/ui/src/stories/Toggle/index.stories.ts
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import Toggle from './component'
+
+const meta: Meta<typeof Toggle> = {
+  title: 'components/Toggle',
+  component: Toggle,
+  tags: ['autodocs'],
+  argTypes: {}
+}
+
+export default meta
+
+type Story = StoryObj<typeof Toggle>
+
+export const Base: Story = {}
