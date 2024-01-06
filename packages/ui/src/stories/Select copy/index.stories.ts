b933524210892d8de363a134b5bd90f14f6c7f95
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-06 23:37:44 +0200

    refactor(frontend ui-package): created separator storybook

---
+++ packages/ui/src/stories/Select copy/index.stories.ts
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import Separator from './component'
+
+const meta: Meta<typeof Separator> = {
+  title: 'components/Separator',
+  component: Separator,
+  tags: ['autodocs'],
+  argTypes: {}
+}
+
+export default meta
+
+type Story = StoryObj<typeof Separator>
+
+export const Base: Story = {}
