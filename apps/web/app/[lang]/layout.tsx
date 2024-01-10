3ef87e3076bd2288506a873110cadd7cb7ca2e6a
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-11 01:20:53 +0200

    refactor(frontend seo): added metadata to different languages

--- apps/web/app/[lang]/layout.tsx
+++ apps/web/app/[lang]/layout.tsx
@@ -4,15 +4,53 @@ import type { Metadata } from 'next'
 import { i18n, Locale } from '@root/i18n.config'
 import SiteFooter from '@web/components/site-footer'
 import SiteHeader from '@web/components/site-header'
+import { getDictionary } from '@web/lib/dictionary'
 
 import { cn, ThemeProvider } from '@repo/ui/util'
 
-export const metadata: Metadata = {
-  title: {
-    template: `%s | Barber Byte`,
-    default: 'Barber Byte'
-  },
-  description: ''
+export async function generateMetadata({ params }: { params: { lang: Locale } }) {
+  const { home } = await getDictionary(params.lang)
+
+  return {
+    title: {
+      template: `%s | Barber Byte`,
+      default: 'Barber Byte'
+    },
+    description: home.metadata.description,
+    // metadataBase: new URL(siteConfig.url),
+    keywords: home.metadata.keywords,
+    authors: home.metadata.authors,
+    // creator: '',
+    // openGraph: {
+    //   type: 'website',
+    //   locale: 'en_US',
+    //   url: siteConfig.url,
+    //   title: siteConfig.name,
+    //   description: siteConfig.description,
+    //   siteName: siteConfig.name,
+    //   images: [
+    //     {
+    //       url: siteConfig.ogImage,
+    //       width: 1200,
+    //       height: 630,
+    //       alt: siteConfig.name
+    //     }
+    //   ]
+    // },
+    // twitter: {
+    //   card: 'summary_large_image',
+    //   title: siteConfig.name,
+    //   description: siteConfig.description,
+    //   images: [siteConfig.ogImage],
+    //   creator: '@'
+    // },
+    icons: {
+      icon: '/favicon.ico',
+      shortcut: '/favicon-16x16.png',
+      apple: '/apple-touch-icon.png'
+    }
+    // manifest: `${siteConfig.url}/site.webmanifest`
+  }
 }
 
 export default function RootLayout({
