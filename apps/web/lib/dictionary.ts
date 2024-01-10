7232571010914167d70a9213c4adf4ef74b03add
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-11 00:30:02 +0200

    refactor(root): added simplified chinese language

--- apps/web/lib/dictionary.ts
+++ apps/web/lib/dictionary.ts
@@ -4,7 +4,8 @@ import type { Locale } from '@root/i18n.config'
 
 const dictionaries = {
   en: () => import('@web/dictionaries/en.json').then((module) => module.default),
-  ar: () => import('@web/dictionaries/ar.json').then((module) => module.default)
+  ar: () => import('@web/dictionaries/ar.json').then((module) => module.default),
+  zh_HANS: () => import('@web/dictionaries/zh_HANS.json').then((module) => module.default)
 }
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]()
--- i18n.config.ts
+++ i18n.config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
   defaultLocale: 'en',
-  locales: ['en', 'ar']
+  locales: ['en', 'ar', 'zh_HANS']
 } as const
 
 export type Locale = (typeof i18n)['locales'][number]
