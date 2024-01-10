191cc9a7382875828d231f3efc7ce508018fc7a0
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-10 22:32:04 +0200

    refactor(frontend web): added header languages

--- apps/web/app/[lang]/layout.tsx
+++ apps/web/app/[lang]/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang={params.lang} suppressHydrationWarning>
+      <head />
+
       <body className={cn('bg-background min-h-screen font-sans antialiased')}>
         <ThemeProvider
           attribute="class"
@@ -33,7 +35,7 @@ export default function RootLayout({
         >
           <div vaul-drawer-wrapper="">
             <div className="bg-background relative flex min-h-screen flex-col">
-              <SiteHeader />
+              <SiteHeader params={params} />
               <main className="container flex-1">{children}</main>
               <SiteFooter params={params} />
             </div>
--- apps/web/components/site-header.tsx
+++ apps/web/components/site-header.tsx
@@ -1,18 +1,18 @@
+import { Locale } from '@root/i18n.config'
+import { getDictionary } from '@web/lib/dictionary'
+
 import Header from '@repo/ui/header'
 
-export default function SiteHeader() {
+export default async function SiteFooter({
+  params: { lang }
+}: {
+  params: { lang: Locale }
+}) {
+  const { header: metaData } = await getDictionary(lang)
+
   return (
     <>
-      <Header
-        metaData={{
-          name: 'Barber Byte',
-          img_url: '/icons/logo.svg',
-          links: [
-            { name: 'Home', path: '/' },
-            { name: 'Feed', path: '/feed' }
-          ]
-        }}
-      />
+      <Header metaData={metaData} />
     </>
   )
 }
