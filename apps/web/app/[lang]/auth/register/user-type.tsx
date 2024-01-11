321f9da7f439e6364d7dd61eed8b2a39339a2dfb
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-12 01:38:36 +0200

    refactor(frontend web): added icons to user type form

--- apps/web/app/[lang]/auth/register/user-type.tsx
+++ apps/web/app/[lang]/auth/register/user-type.tsx
@@ -1,3 +1,34 @@
+import { ReactElement } from 'react'
+
+import { Button } from '@repo/ui/button'
+import { Home, Person } from '@repo/ui/icons'
+import { cn } from '@repo/ui/util'
+
 export default function Index() {
-  return <div>User or Barber Shop</div>
+  const userTypes: { name: string; icon: ReactElement }[] = [
+    { name: 'User', icon: <Person className={'h-32 w-32'} /> },
+    { name: 'Barber', icon: <Home className={'h-32 w-32'} /> }
+  ]
+
+  return (
+    <div className="flex items-center justify-center gap-6">
+      {userTypes?.map((item, index) => {
+        return (
+          <Button
+            aria-label={item?.name}
+            key={index}
+            variant={'outline'}
+            className={cn(
+              'relative h-fit w-fit hover:text-amber-900 ',
+              'flex flex-col gap-1 rounded-xl'
+            )}
+          >
+            {item?.icon}
+
+            <span className={'w-full text-center text-xl'}>{item?.name}</span>
+          </Button>
+        )
+      })}
+    </div>
+  )
 }
