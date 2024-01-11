3fe6930e628b9dcafd023cde198aa9ab0dbff3ee
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-11 19:53:38 +0200

    refactor(frontend ui-package): added layoutTextImage props to storybook

--- packages/ui/src/stories/Components/HeroLayout/index.stories.ts
+++ packages/ui/src/stories/Components/HeroLayout/index.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import Layout from './component'
+import Layout from '../../../elements/layoutTextImage'
+import PostImage from '../../@assets/default-img.png'
 
 const meta: Meta<typeof Layout> = {
   title: 'components/Layout',
@@ -8,10 +9,33 @@ const meta: Meta<typeof Layout> = {
   tags: ['autodocs'],
   parameters: {
     layout: 'fullscreen'
+  },
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['default', 'description']
+    }
   }
 }
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Base: Story = {}
+export const Default: Story = {
+  args: {
+    variant: 'default',
+    img_url: PostImage,
+    title: 'Lorem Ipsum is simply dummy text',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    reverse: false
+  }
+}
+
+export const Description: Story = {
+  args: {
+    variant: 'description',
+    img_url: PostImage,
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    reverse: false
+  }
+}
