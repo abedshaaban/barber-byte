a17d536767efbdbbcb51af0f71742422ae65e78b
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-12 17:40:29 +0200

    docs(frontend web): added documentation to the checkEmailFormat helper function

--- apps/web/helpers/index.tsx
+++ apps/web/helpers/index.tsx
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param email
+ * @returns `true` if the string matches the email format
+ */
 export function checkEmailFormat(email: string): boolean {
   return email.match(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/) ? true : false
 }
