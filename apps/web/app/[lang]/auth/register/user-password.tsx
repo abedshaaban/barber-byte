d5896c31cff91fb76661a915ea6ea1a9a8e7bb87
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-13 01:49:07 +0200

    refactor(frontend web): toggle show and hide password

--- apps/web/app/[lang]/auth/register/user-password.tsx
+++ apps/web/app/[lang]/auth/register/user-password.tsx
@@ -1,5 +1,17 @@
+import { useState } from 'react'
+
+import { Button } from '@repo/ui/button'
+import {
+  AtMark,
+  ClosedEye,
+  EightPlus,
+  LowerCaseLetter,
+  OpenEye,
+  UpperCaseLetter
+} from '@repo/ui/icons'
 import { Input } from '@repo/ui/input'
 import { Label } from '@repo/ui/label'
+import { cn } from '@repo/ui/util'
 
 type UserData = {
   password: string
@@ -10,21 +22,38 @@ type UserPasswordProps = UserData & {
 }
 
 export default function Index({ password, updateFields }: UserPasswordProps) {
+  const [showPassword, setShowPassword] = useState(false)
+
   return (
     <div className={'grid w-full max-w-sm items-center gap-1.5'}>
       <Label htmlFor={'email'}>Password</Label>
-      <Input
-        type={'password'}
-        id={'email'}
-        placeholder={'********'}
-        min={8}
-        minLength={8}
-        required
-        value={password}
-        onChange={(e) => {
-          updateFields({ password: e.target.value })
-        }}
-      />
+
+      <div className="flex w-full">
+        <Input
+          className={cn('rounded-r-none border-r-0 outline-none focus-visible:ring-0')}
+          type={showPassword ? 'text' : 'password'}
+          id={'email'}
+          placeholder={'********'}
+          min={8}
+          minLength={8}
+          required
+          value={password}
+          onChange={(e) => {
+            updateFields({ password: e.target.value })
+          }}
+        />
+
+        <Button
+          variant={'outline'}
+          size={'icon'}
+          className={cn('rounded-l-none border-l-0')}
+          onClick={() => {
+            setShowPassword(!showPassword)
+          }}
+        >
+          {showPassword ? <OpenEye /> : <ClosedEye />}
+        </Button>
+      </div>
     </div>
   )
 }
