edc262cc114fecc3b15f1c00c7d373d3f8245408
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-13 21:08:45 +0200

    refactor(frontend helpers-package): created register props

--- packages/helpers/src/types/index.d.ts
+++ packages/helpers/src/types/index.d.ts
@@ -2,3 +2,11 @@ export type LoginProps = {
   email: string
   password: string
 }
+
+export type RegisterProps = {
+  email: string
+  password: string
+  first_name: string
+  last_name: string
+  birth_date: Date
+}
