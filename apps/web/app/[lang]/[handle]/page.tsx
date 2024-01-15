554d09d98f431531212b4e94149b531606305c9b
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-15 23:26:27 +0200

    refactor(frontend web): added meta data for user page

--- apps/web/app/[lang]/[handle]/page.tsx
+++ apps/web/app/[lang]/[handle]/page.tsx
@@ -1,7 +1,38 @@
+import type { Metadata } from 'next'
+import type { UserType } from '@repo/helpers/types'
 import { Locale } from '@root/i18n.config'
+import { getProfileByHandle } from '@root/packages/helpers/src/account'
 import { getDictionary } from '@web/lib/dictionary'
 
 import Logic from './logic'
+
+export async function generateMetadata({
+  params
+}: {
+  params: { handle: string }
+}): Promise<Metadata> {
+  const handle = params.handle.substring(3)
+
+  const { data: user }: { data: UserType } = await getProfileByHandle({
+    handle: handle
+  })
+
+  let meta = {}
+
+  if (user?.role === 'user') {
+    meta = {
+      title: `${user.first_name} ${user.last_name}`,
+      description: user.description
+    }
+  } else if (user?.role === 'shop') {
+    meta = {
+      title: user.shop_name,
+      description: user.description
+    }
+  }
+
+  return meta
+}
 
 export default async function Page({
   params
@@ -9,7 +40,7 @@ export default async function Page({
   params: { handle: string; lang: Locale }
 }) {
   const handle = params.handle.substring(3)
-  const {navigation} = await getDictionary(params.lang)
+  const { navigation } = await getDictionary(params.lang)
 
   return (
     <>
