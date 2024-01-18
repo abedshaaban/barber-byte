b1fa118910dfb3f983f24d7c078531d09bb9728c
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-19 00:28:51 +0200

    refactor(backend): created post model

---
+++ apps/backend/app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'uuid';
+    protected $keyType = 'uuid';
+
+    protected $fillable = [
+        'uuid',
+        'caption',
+        'img_url',
+        'likes_count',
+        'creator_id',
+    ];
+}
