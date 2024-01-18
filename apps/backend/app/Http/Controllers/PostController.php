7bc97529e9ecce82d2df660abee38656b3785e65
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-19 01:02:52 +0200

    refactor(backend): created post function

--- apps/backend/app/Http/Controllers/PostController.php
+++ apps/backend/app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -11,5 +12,46 @@ class PostController extends Controller
 
     public function __construct(){
         $this->user = Auth::user();
+    }
+
+    public function create_post(Request $request){
+        try{
+            $request->validate([
+                'img_url' => 'required|image|mimes:jpeg,png,jpg,webp',
+            ]);
+
+            $image_name = time() . '.' . $request->img_url->extension();
+
+            $folder_path = 'images/posts/' . $this->user->uuid;
+            
+            if (!file_exists(public_path($folder_path))) {
+                mkdir(public_path($folder_path), 0755, true);
+            }
+            
+            $request->img_url->move(public_path($folder_path), $image_name);
+            
+
+            $post = Post::
+                create([
+                    'img_url' => $folder_path . '/' . $image_name,
+                    'caption' => $request->caption,
+                    'creator_id' => $this->user->uuid
+                ]);
+            
+            return response()->json([
+                'status' => true,
+                'message' => 'Post created successfully',
+                'data' => $post,
+                'error' => ''
+            ]);
+
+        }catch(\Exception $exception){
+            return response()->json([
+                'status' => false,
+                'message' => 'Error occurred while creating post.',
+                'data' => '',
+                'error' => $exception->getMessage() 
+            ]);
+        }
     }
 }
