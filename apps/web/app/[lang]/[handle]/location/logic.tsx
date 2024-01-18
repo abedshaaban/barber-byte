82a46a85ab6c5b01f307c6b074123a6af24c1d31
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-18 02:41:50 +0200

    refactor(frontend  web): created shop location page

---
+++ apps/web/app/[lang]/[handle]/location/logic.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { Logout } from '@repo/helpers/auth'
+import type { UserType } from '@repo/helpers/types'
+import { Locale } from '@root/i18n.config'
+import type { RootState } from '@web/provider/store'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { Button, buttonVariants } from '@repo/ui/button'
+
+export default function Logic({
+  handle,
+  lang,
+  navigationText,
+  profile: initial_profile
+}: {
+  handle: string
+  lang: Locale
+  navigationText: any
+  profile: UserType | null
+}) {
+  const dispatch = useDispatch()
+  const user = useSelector((state: RootState) => state.user.user)
+  const [profile, setProfile] = useState<UserType | null>(initial_profile)
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+
+  useEffect(() => {
+    if (user?.handle === handle) {
+      setProfile(user)
+    }
+  }, [user])
+
+  return (
+    <>
+      {!profile ? (
+        <div className={'flex h-full w-full flex-col items-center justify-center gap-9'}>
+          <h1 className={'text-2xl md:text-5xl'}>( • ᴖ • ｡) Location Not Found</h1>
+
+          <p>
+            Location page is not found. Go{' '}
+            <Link href={`/`} className={'underline'}>
+              to home
+            </Link>
+          </p>
+        </div>
+      ) : (
+        <section className={'flex flex-col gap-9'}>location</section>
+      )}
+    </>
+  )
+}
