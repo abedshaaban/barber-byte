5ef1f5e504803373d98b08bde8c200e50dfe9cc9
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-19 16:31:37 +0200

    refactor(frontend helpers-package): created a get posts function

--- packages/helpers/src/account/index.ts
+++ packages/helpers/src/account/index.ts
@@ -166,3 +166,33 @@ export async function createPost({
 
   return res?.data
 }
+
+/**
+ * Get a post
+ * @param page optional, for pagination
+ */
+export async function getPosts({ page }: { page: number }): Promise<{
+  status: boolean
+  data: any
+  error: string
+  message: string
+}> {
+  let res
+
+  try {
+    res = await axios.get(
+      `${process.env.NEXT_PUBLIC_DB_URL_APIS}/post/get?page=${page}`,
+
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+  } catch (error) {
+    console.error(error)
+  }
+
+  return res?.data
+}
