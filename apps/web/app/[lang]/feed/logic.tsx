726521a680fb67c9f524b32accbccbde2a405782
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-19 16:47:14 +0200

    refactor(frontend web): get post from backend

--- apps/web/app/[lang]/feed/logic.tsx
+++ apps/web/app/[lang]/feed/logic.tsx
@@ -1,15 +1,42 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+import { getPosts } from '@repo/helpers/account'
 import Post from '@web/components/post'
 import { RootState } from '@web/provider/store'
 import { useSelector } from 'react-redux'
 
 export default function Logic() {
   const user = useSelector((state: RootState) => state.user)
+  const [pageNumber, setPageNumber] = useState<{ currentPage: number; lastPage: number }>(
+    { currentPage: 1, lastPage: 1 }
+  )
+  const [posts, setPosts] = useState([])
+
+  async function getData() {
+    if (pageNumber.currentPage <= pageNumber.lastPage) {
+      const res = await getPosts({ page: pageNumber.currentPage })
+
+      setPosts((prev) => {
+        return [...prev, ...res.data]
+      })
+    }
+  }
+
+  useEffect(() => {
+    const runFunc = async () => {
+      await getData()
+    }
+
+    runFunc()
+  }, [])
 
   return (
     <div className={'flex w-full flex-col items-center justify-center'}>
       <Post />
+      {posts?.map((val) => {
+        return <>{val?.handle}</>
+      })}
     </div>
   )
 }
