426c78a0d1e540d22d9107c4b3414897e59d8c1e
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-21 21:49:07 +0200

    refactor(frontend web): added translation to generate image card

--- apps/web/app/[lang]/reserve-appointment/generate-image.tsx
+++ apps/web/app/[lang]/reserve-appointment/generate-image.tsx
@@ -1,17 +1,9 @@
-import { text } from 'stream/consumers'
 import type { AppointmentType } from '@repo/helpers/types'
 
 import { Input } from '@repo/ui/input'
 import { Label } from '@repo/ui/label'
+import { Textarea } from '@repo/ui/textarea'
 import { cn } from '@repo/ui/util'
-
-type formDataProps = {
-  name: string
-  label: string
-  placeholder?: string
-  type: string
-  value: string | number | undefined
-}
 
 type UserFormProps = Partial<AppointmentType> & {
   updateFields: (fields: Partial<AppointmentType>) => void
@@ -24,28 +16,11 @@ export default function Index({
   updateFields,
   reservation
 }: UserFormProps) {
-  const formData: formDataProps[] = [
-    {
-      name: 'client_name',
-      label: reservation.clientData.name,
-      type: 'text',
-      value: client_name
-    },
-    {
-      name: 'description',
-      label: reservation.clientData.description,
-      type: 'text',
-      placeholder: reservation.clientData.descriptionExample,
-      value: description
-    }
-  ]
-
   return (
     <div className={cn('flex flex-col gap-6')}>
       <div className="grid w-full max-w-sm items-center gap-1.5">
         <Label htmlFor={'prompt'}>{reservation.imageGeneration.prompt}</Label>
-        <Input
-          type={'text'}
+        <Textarea
           id={'prompt'}
           placeholder={reservation.imageGeneration.promptExample}
           required
