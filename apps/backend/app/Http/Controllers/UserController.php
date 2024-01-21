68a3a4dfb26777a1db9be72e2411faf9eeac69f1
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-21 14:55:28 +0200

    refactor(backend): updated update profile function

--- apps/backend/app/Http/Controllers/UserController.php
+++ apps/backend/app/Http/Controllers/UserController.php
@@ -7,7 +7,6 @@ use App\Models\Shop;
 use App\Models\Address;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -41,40 +40,15 @@ class UserController extends Controller
             }
         }
 
-
-        if($this->user['role_id'] === 1){
-
-            $db_user = User::
-                select(
-                    'first_name',
-                    'last_name',
-                    'birth_date',
-                    'description',
-                    'img_url',
-                    'gender_id',
-                    'account_status_id'
-                )
-                    ->where('email', $this->user['email'])->first();
-                            
+        if($this->user->role->name === 'user'){
             $this->user->update([
-                'first_name' => $request->first_name ?? $db_user['first_name'],
-                'last_name' => $request->last_name ?? $db_user['last_name'],
-                'birth_date' => $request->birth_date ?? $db_user['birth_date'],
-                'description' => $request->description ?? $db_user['description'],
-                'gender_id' => $request->gender ?? $db_user['gender_id'],
-                'account_status_id' => $request->account_status ?? $db_user['account_status_id']
+                'first_name' => $request->first_name ?? $this->user->first_name,
+                'last_name' => $request->last_name ?? $this->user->last_name,
+                'birth_date' => $request->birth_date ?? $this->user->birth_date,
+                'description' => $request->description ?? $this->user->description,
+                'gender_id' => $request->gender ?? $this->user->gender->id,
+                'account_status_id' => $request->account_status ?? $this->user->account_status->id
             ]);
-
-            $db_user_meta_data = User::
-            select(
-                'roles.name as role_name',
-                'genders.name as gender_name',
-                'account_statuses.name as account_status',
-            )
-            ->join('roles', 'users.role_id','=','roles.id')
-            ->join('genders', 'users.gender_id','=','genders.id')
-            ->join('account_statuses', 'users.account_status_id','=','account_statuses.id')
-            ->where('email', $this->user['email'])->first();
             
             $res = [
                 'status' => true,
@@ -86,29 +60,18 @@ class UserController extends Controller
                     'birth_date' => $this->user->birth_date,
                     'description' => $this->user->description,
                     'img_url' => $this->user->img_url,
-                    'role' => $db_user_meta_data->role_name,
-                    'gender' => $db_user_meta_data->gender_name,
-                    'account_status' => $db_user_meta_data->account_status,
+                    'role' => $this->user->role->name,
+                    'gender' => $this->user->gender->name,
+                    'account_status' => $this->user->account->name,
                 ],
                 'error' => '' 
             ];
-        } else if ($this->user['role_id'] === 2){
-            
-            $db_user = User::
-            select(
-                'birth_date',
-                'description',
-                'img_url',
-                'gender_id',
-                'account_status_id'
-            )
-                ->where('email', $this->user['email'])->first();
-
+        } else if ($this->user->role->name === 'shop'){
             $this->user->update([
-                'birth_date' => $request->birth_date ?? $db_user['birth_date'],
-                'description' => $request->description ?? $db_user['description'],
-                'gender_id' => $request->gender ?? $db_user['gender_id'],
-                'account_status_id' => $request->account_status ?? $db_user['account_status_id']
+                'birth_date' => $request->birth_date ?? $this->user->birth_date,
+                'description' => $request->description ?? $this->user->description,
+                'gender_id' => $request->gender ?? $this->user->gender->id,
+                'account_status_id' => $request->account_status ?? $this->user->account_status->id
             ]);
 
             $shop = Shop::find($this->user['uuid']);
@@ -123,18 +86,6 @@ class UserController extends Controller
                 'location' => $request->location ?? $address['location'],
             ]);
 
-            $db_user_meta_data = User::
-            select(
-                'roles.name as role_name',
-                'genders.name as gender_name',
-                'account_statuses.name as account_status',
-            )
-            ->join('roles', 'users.role_id','=','roles.id')
-            ->join('genders', 'users.gender_id','=','genders.id')
-            ->join('account_statuses', 'users.account_status_id','=','account_statuses.id')
-            ->where('email', $this->user['email'])->first();
-
-
             $res = [
                 'status' => true,
                 'message' => 'Updated user successfully',
@@ -143,19 +94,19 @@ class UserController extends Controller
                     'birth_date' => $this->user->birth_date,
                     'description' => $this->user->description,
                     'img_url' => $this->user->img_url,
-                    'role' => $db_user_meta_data->role_name,
-                    'gender' => $db_user_meta_data->gender_name,
-                    'account_status' => $db_user_meta_data->account_status,
-                    'shop_name' => $shop->name,
-                    'country' => $address->country,
-                    'city' => $address->city,
-                    'street' => $address->street,
-                    'location' => json_decode($address->location)
+                    'role' => $this->user->role->name,
+                    'gender' => $this->user->gender->name,
+                    'account_status' => $this->user->account_status->name,
+                    'shop_name' => $this->user->shop->name,
+                    'country' => $this->user->shop->address->country,
+                    'city' => $this->user->shop->address->city,
+                    'street' => $this->user->shop->address->street,
+                    'location' => json_decode($this->user->shop->address->location),
+                    'work_days' => $this->user->shop->work_days
                 ],
                 'error' => '' ,
             ];
         }
-
 
         return response()->json($res);
     }
