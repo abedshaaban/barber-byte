1e4de43403c92fab4f03a28f48602c9aac39ed03
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-22 21:24:13 +0200

    refactor(backend): added date and updated time in reservation migration

--- apps/backend/database/migrations/2024_01_22_181240_create_reservations_table.php
+++ apps/backend/database/migrations/2024_01_22_181240_create_reservations_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('time');
+            $table->string('time', 5);
+            $table->date('date');
             $table->foreignId('img_id')
             ->references('id')
             ->on('ai_images');
