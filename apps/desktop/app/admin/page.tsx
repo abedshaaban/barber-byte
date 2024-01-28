31e24ea1f9f29a7cce2c3d799f978e53d668b8e2
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-01-28 20:01:43 +0200

    refactor(desktop): display total users for admin

--- apps/desktop/app/admin/page.tsx
+++ apps/desktop/app/admin/page.tsx
@@ -1,3 +1,65 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { GetNumberOfUsers } from '@desktop/helpers'
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@repo/ui/card'
+
 export default function Page() {
-  return <div>Admin</div>
+  const [users, setUsers] = useState<{ total_users: number; deleted_users: number }>({
+    total_users: 0,
+    deleted_users: 0
+  })
+
+  useEffect(() => {
+    async function getData() {
+      const res = await GetNumberOfUsers()
+      console.log(res.data.count)
+
+      let c = 0
+      for (let i = 0; i < res.data.count.length; i++) {
+        c += res.data.count[i]
+      }
+
+      setUsers((prev) => {
+        return { ...prev, total_users: c }
+      })
+    }
+
+    getData()
+  }, [])
+
+  return (
+    <section className={'flex w-full flex-col justify-start pt-6'}>
+      <h1 className={'text-2xl font-semibold'}>Home</h1>
+
+      <div className={'py-9'}>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Total Users</CardTitle>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="text-muted-foreground h-4 w-4"
+              >
+                <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                <circle cx="9" cy="7" r="4" />
+                <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+              </svg>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{users?.total_users}</div>
+              <p className="text-muted-foreground text-xs">All time</p>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </section>
+  )
 }
--- apps/desktop/app/shop/page.tsx
+++ apps/desktop/app/shop/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 
+import React, { useEffect, useState } from 'react'
+import { GetNumberOfUsers } from '@desktop/helpers'
+
 export default function Page() {
   return (
-    <div>
-      <div>Shop</div>
-    </div>
+    <section className={'flex w-full flex-col justify-start pt-6'}>
+      <h1 className={'text-2xl font-semibold'}>Home</h1>
+
+      <div className={'py-9'}></div>
+    </section>
   )
 }
